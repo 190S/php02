7cd178ca63f22da9f9f67d707fe20acd7fe006fc
vagrant <vagrant@vagrant.vm>
2018-12-19 13:32:59 +0000

    php02_kadai

---
+++ kadai3.php
@@ -0,0 +1,17 @@
+<?php
+
+//$helloという変数に”Hello, “を
+//$name という変数にあなたの名前を
+//$world という変数に”‘s World!” を代入して、
+//各変数を連結させて “Hello, あなたの名前’s World!” と表示してみましょう。
+
+//$helloにHello,を代入
+$hello = "Hello, ";
+//nameにIkuoを代入
+$name = "Ikuo";
+//worldに's World!を代入
+$world = "’s World!";
+
+//$hello $name $worldを並べて表示.で文字を連結
+echo $hello.$name.$world;
+ ?>
---
+++ kadai4.php
@@ -0,0 +1,14 @@
+<?php
+//$tech_boostに “tech “という文字列が代入されています。
+//複合演算を使って、echo $tech_boostと実行すると
+//「tech boost」と表示されるように実装してみましょう。
+
+//$tech_boostに"tech "が代入されている
+$tech_boost = "tech ";
+
+//$tech_boostに.=で"boost"を連結させる（複合演算)
+$tech_boost .= "boost";
+
+//結果の出力
+echo $tech_boost
+ ?>
